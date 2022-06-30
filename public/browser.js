ad3ffc009338aefc28bb9f22ea1a467179c5066a
starlabelgh <hello@starlabel.co>
2022-06-30 11:36:38 +0000

    configured the delete button to delete an item from the mongo database

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,15 @@
 document.addEventListener("click", function(e){
+    //delete feature
+    if(e.target.classList.contains("delete-me")){
+        if (confirm("Do you really want to delete this item permanently")){
+            axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(function(){
+                e.target.parentElement.parentElement.remove()
+           }).catch(function(){
+            console.log("Please try again later")
+           })
+        }
+    }
+    //update feature
     if(e.target.classList.contains("edit-me")){
        let userInput = prompt("enter your desired new test", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
        if (userInput){
